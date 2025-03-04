a7b3f2c8e477a518271e01188b24a0d369f12b91
Tim Dove <timdoveemail@yahoo.com>
2025-03-04 13:08:23 -0500

    Preparing to deeploy to VPS.

---
+++ src/app/navy/page.tsx
@@ -0,0 +1,12 @@
+import NavalPowerChart from "./navyRankByYear"
+import React from 'react';
+
+const TestPage: React.FC = () => {
+    return (
+        <div>
+            <NavalPowerChart />
+        </div>
+    );
+};
+
+export default TestPage;
