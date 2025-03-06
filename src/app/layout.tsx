ad45b31ee499903414cfcdcd2d993708d4e94ff6
Tim Dove <timdoveemail@yahoo.com>
2025-03-05 20:23:14 -0500

    Added CLerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,28 @@
-import type { Metadata } from "next"
-// import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
+import { type Metadata } from 'next'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 
-// const geistSans = Geist({
-// variable: "--font-geist-sans",
-// subsets: ["latin"],
-// })
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// })
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: "onlyLinks",
-  description: "Crytpo DeFi trading.",
+  description: "All the good links.",
   // icons: {
   //   icon: "/favicon.ico",
   // },
@@ -22,12 +30,25 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
